3b8fbb7aeb502295ac02ff5a8567add94d171441
Joey Cho <cjy12182000@naver.com>
2018-01-31 18:18:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+function logDriverNames(drivers){
+  drivers.forEach(function(driver){
+  console.log(driver.name);
+});
+}
+
+function logDriversByHometown(drivers,location){
+  drivers.forEach(function(driver){
+    if(driver.hometown === location){
+      console.log(driver.name)
+      }
+  })
+}
+
+function driversByRevenue(drivers){
+  orddrivers = drivers
+  orddrivers.sort(function(dr1,dr2){
+    return dr1['revenue'] - dr2['revenue'];
+  });
+  return orddrivers;
+}
